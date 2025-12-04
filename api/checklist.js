98ee4ad4fe33afbda44c5d66c89cfd151391eff0
msentregaspgm-bit <msentregaspgm@gmail.com>
2025-12-04 09:35:23 -0300

    Update checklist.js

--- api/checklist.js
+++ api/checklist.js
@@ -1,5 +1,6 @@
 export default async function handler(req, res) {
-  const TARGET_URL = "https://script.google.com/macros/s/AKfycbyHPCOhlA55vbvak_KqdIEZtSBqRtvBC1l1kTY5vjN1tGm36arLV9GcJrfnzpwNS5-m/exec";
+  const TARGET_URL =
+    "https://script.google.com/macros/s/AKfycbyJbwS4d9c14RHrTNBjbrRlTQfZg3728tzSDOvH_kxBvenJrD4xn1wS7UGJsh7nS3VE/exec";
 
   try {
     const response = await fetch(TARGET_URL, {
@@ -9,7 +10,6 @@ export default async function handler(req, res) {
     });
     const text = await response.text();
     res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Methods", "POST,OPTIONS");
     res.status(200).json({ result: text });
   } catch (err) {
     console.error("Erro no proxy:", err);
